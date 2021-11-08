e31f809735c6ccdb5cf7b4fb5d01b80382e62b6d
Daniel Bergey <bergey@simspace.com>
2021-11-08 08:49:10 -0500

    firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import './App.css';
-import React, { useState } from 'react'
+
+/* import { getAnalytics } from "firebase/analytics"; */
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs, addDoc } from 'firebase/firestore/lite';
+import React, { useEffect, useState } from 'react'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAvatZfZuSG49YPSN17INahQX6aokYG9q8",
+    authDomain: "books-331420.firebaseapp.com",
+    projectId: "books-331420",
+    storageBucket: "books-331420.appspot.com",
+    messagingSenderId: "97831789532",
+    appId: "1:97831789532:web:24d92bf9be475802c72519",
+    measurementId: "G-29SXV008TY"
+};
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
+/* const analytics = getAnalytics(firebase); */
+const db = getFirestore(firebase);
 
 const e = React.createElement;
 
@@ -11,6 +29,11 @@ function App() {
   const emptyInput = {title: '', author: '' }
   const [input, setInput] = useState(emptyInput)
 
+  useEffect( () => ( async () => {
+      const upstream = await getDocs(collection(db, 'books'))
+      setBooks(upstream.docs.map(d => d.data()))
+  })(), [])
+
   const rows = books.map(book =>
       e ('tr', {}, e('td', {}, book.title), e('td', {}, book.author), e('td')))
 
@@ -21,10 +44,12 @@ function App() {
       </td>
   )
 
-  const addRow = () => {
+  const addRow = () => (async () => {
       setBooks([...books, input])
+      const bookRef = await addDoc(collection(db, 'books'), input)
+      console.log(`bookRef=${bookRef}`)
       setInput(emptyInput)
-  }
+  })()
 
   return (
       <table>
