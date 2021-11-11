bb2893ac4e76a08381dd99bfaebb5fdfdb2fe35d
Daniel Bergey <bergey@simspace.com>
2021-11-11 08:56:29 -0500

    prettier

--- src/App.js
+++ src/App.js
@@ -1,85 +1,102 @@
-import './App.css';
+import './App.css'
 import { db, signIn, onAuthStateChanged } from './firebase'
 
-import { collection, doc, getDocs, addDoc, deleteDoc } from 'firebase/firestore/lite';
+import { collection, doc, getDocs, addDoc, deleteDoc } from 'firebase/firestore/lite'
 import React, { useEffect, useState } from 'react'
 
 function Books() {
   const [books, setBooks] = useState([])
-  const emptyInput = {title: '', author: '' }
+  const emptyInput = { title: '', author: '' }
   const [input, setInput] = useState(emptyInput)
 
-  useEffect( () => ( async () => {
-      const upstream = await getDocs(collection(db, 'books'))
-      setBooks(upstream.docs.map(d => ({...d.data(), key: d.id})))
-  })(), [])
+  useEffect(
+    () =>
+      (async () => {
+        const upstream = await getDocs(collection(db, 'books'))
+        setBooks(upstream.docs.map(d => ({ ...d.data(), key: d.id })))
+      })(),
+    []
+  )
 
-  const deleteBook = (key) => () => (async () => {
+  const deleteBook = key => () =>
+    (async () => {
       console.log(`key=${key}`)
       await deleteDoc(doc(db, 'books', key))
-      setBooks(books.filter( b => b.key !== key))
+      setBooks(books.filter(b => b.key !== key))
     })()
 
   const rows = books.map(book => (
-      <tr key={book.key}>
-        <td>{book.title}</td>
-        <td>{book.author}</td>
-        <td><button onClick={deleteBook(book.key)}>delete</button></td>
-      </tr>
+    <tr key={book.key}>
+      <td>{book.title}</td>
+      <td>{book.author}</td>
+      <td>
+        <button onClick={deleteBook(book.key)}>delete</button>
+      </td>
+    </tr>
   ))
 
   const inputCell = name => (
-      <td>
-        <input name={name} type="text" value={input[name]}
-          onChange={ev => setInput({...input, [name]: ev.target.value })} />
-      </td>
+    <td>
+      <input
+        name={name}
+        type='text'
+        value={input[name]}
+        onChange={ev => setInput({ ...input, [name]: ev.target.value })}
+      />
+    </td>
   )
 
-  const addRow = () => (async () => {
+  const addRow = () =>
+    (async () => {
       let newBooks = [...books, input]
       setBooks(newBooks)
       const bookRef = await addDoc(collection(db, 'books'), input)
       newBooks[newBooks.length - 1].key = bookRef.id
       setBooks(newBooks)
       setInput(emptyInput)
-  })()
+    })()
 
   return (
-      <table>
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {rows}
-          <tr>
-            {inputCell('title')}
-            {inputCell('author')}
-            <td><button onClick={addRow}>+</button></td>
-          </tr>
-        </tbody>
-      </table>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        {rows}
+        <tr>
+          {inputCell('title')}
+          {inputCell('author')}
+          <td>
+            <button onClick={addRow}>+</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   )
-
 }
 
 function App() {
-    const [user, setUser] = useState()
+  const [user, setUser] = useState()
 
-    useEffect(() => {
-        onAuthStateChanged(u => {
-            setUser(u)
-            if (u === undefined) { signIn() }
-        })
-    }, [])
+  useEffect(() => {
+    onAuthStateChanged(u => {
+      setUser(u)
+      if (u === undefined) {
+        signIn()
+      }
+    })
+  }, [])
 
-    return (<>
-        <Books />
-        <p>{user ? `logged in as ${user.displayName}` : 'not logged in'}</p>
-        </>)
+  return (
+    <>
+      <Books />
+      <p>{user ? `logged in as ${user.displayName}` : 'not logged in'}</p>
+    </>
+  )
 }
 
-export default App;
+export default App
--- src/firebase.js
+++ src/firebase.js
@@ -1,34 +1,36 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore/lite';
-import * as Auth from "firebase/auth";
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore/lite'
+import * as Auth from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAvatZfZuSG49YPSN17INahQX6aokYG9q8",
-    authDomain: "books-331420.firebaseapp.com",
-    projectId: "books-331420",
-    storageBucket: "books-331420.appspot.com",
-    messagingSenderId: "97831789532",
-    appId: "1:97831789532:web:24d92bf9be475802c72519",
-    measurementId: "G-29SXV008TY"
-};
+  apiKey: 'AIzaSyAvatZfZuSG49YPSN17INahQX6aokYG9q8',
+  authDomain: 'books-331420.firebaseapp.com',
+  projectId: 'books-331420',
+  storageBucket: 'books-331420.appspot.com',
+  messagingSenderId: '97831789532',
+  appId: '1:97831789532:web:24d92bf9be475802c72519',
+  measurementId: 'G-29SXV008TY',
+}
 // Initialize Firebase
-const firebase = initializeApp(firebaseConfig);
-export const db = getFirestore(firebase);
+const firebase = initializeApp(firebaseConfig)
+export const db = getFirestore(firebase)
 
-export const auth = Auth.getAuth();
-const provider = new Auth.GoogleAuthProvider();
+export const auth = Auth.getAuth()
+const provider = new Auth.GoogleAuthProvider()
 
 // TODO redirect on mobile instead of popup
-export const signIn = () => Auth.signInWithPopup(auth, provider)
+export const signIn = () =>
+  Auth.signInWithPopup(auth, provider)
     .then(result => {
-        console.log(result.user)
-    }).catch((error) => {
-        // Handle Errors here.
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        // The email of the user's account used.
-        const email = error.email;
-        // The AuthCredential type that was used.
-        console.log({errorCode, errorMessage, email})
-    });
+      console.log(result.user)
+    })
+    .catch(error => {
+      // Handle Errors here.
+      const errorCode = error.code
+      const errorMessage = error.message
+      // The email of the user's account used.
+      const email = error.email
+      // The AuthCredential type that was used.
+      console.log({ errorCode, errorMessage, email })
+    })
 export const onAuthStateChanged = callback => Auth.onAuthStateChanged(auth, callback)
